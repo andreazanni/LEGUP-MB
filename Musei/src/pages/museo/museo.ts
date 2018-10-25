f7b4f07669ce822f3428784d99e5cab9b52e2d4b
andreazanni <andrea.zanni1992@gmail.com>
2018-10-25 15:56:10 +0200

    Suddivisione menu principale e menu museo

--- Musei/src/pages/museo/museo.ts
+++ Musei/src/pages/museo/museo.ts
@@ -27,18 +27,23 @@ export class MuseoPage {
     var descrizioneHeight = idCardTitle.offsetHeight - idCardDescrizione.offsetTop;
     idCardDescrizione.style.height = descrizioneHeight + "px";
     //console.log(idCardTitle.offsetTop, idCardTitle.offsetHeight);
+    //Disabilito il menu principale e abilito quello specifico del museo
+    this.menuCtrl.enable(false, "menuPrincipale");
+    this.menuCtrl.enable(true, "menuMuseo");
   }
 
 
  //Apre il side menu
  openMenu() {
-   this.menuCtrl.open();
+   this.menuCtrl.open("menuMuseo");
  }
 
   //Associato al tasto per tornare all'home page
   goHomePage() {
     this.nav.push(HomePage);
     this.nav.removeView(this.nav.last());
-
+    //Riabilito il menu principale e disabilito quello specifico del museo
+    this.menuCtrl.enable(true, "menuPrincipale");
+    this.menuCtrl.enable(false, "menuMuseo");
   }
 }
