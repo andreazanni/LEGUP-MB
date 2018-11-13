2fce76b537590db962f49cd8b3768ba5f45b6310
andreazanni <andrea.zanni1992@gmail.com>
2018-11-13 17:17:44 +0100

    Fix pagina eventi

--- Musei/src/pages/mm-eventi/mm-eventi.ts
+++ Musei/src/pages/mm-eventi/mm-eventi.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, MenuController, Item, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController, LoadingController, Platform, AlertController } from 'ionic-angular';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { HomePage } from '../home/home';
 import { MenuPage } from '../menu/menu';
+import { MuseoPage } from '../museo/museo';
 import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 
 /**
@@ -24,8 +25,10 @@ export class MmEventiPage {
   myMuseoClass: string;
   myContentClass: string;
   myMuseo: any;
+  unregisterBackButtonAction: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public tts: TextToSpeech, public menuCtrl: MenuController, public nativePageTransitions: NativePageTransitions, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public tts: TextToSpeech, public menuCtrl: MenuController, public nativePageTransitions: NativePageTransitions, public loadingCtrl: LoadingController,
+    public platform: Platform, public alertCtrl: AlertController) {
 
     this.myVoceMenu = this.navParams.get('voceMenu');
     this.myContenuto = this.navParams.get('contenuto');
@@ -36,7 +39,6 @@ export class MmEventiPage {
 
   ionViewDidLoad() {
      // personalizzo la pagina di contenuto in base al museo e alla voce di menù selezionata
-     console.log(this.myContenuto);
      var idClass = document.getElementById('paginaMmEventi');
      idClass.classList.add(this.myMuseoClass);
 
@@ -44,7 +46,6 @@ export class MmEventiPage {
      document.getElementById('content_cardSubTitle').innerText = this.myMuseo[0].NOME;
 
      var idCardContenuto = document.getElementById('contenuto');
-     console.log(idCardContenuto);
      idCardContenuto.innerHTML = this.myContenuto;
 
      var idContainerContenuto = document.getElementById('container-contenuto');
@@ -52,12 +53,9 @@ export class MmEventiPage {
      //calcolo l'altezza del contenitore contenuto sottraendo dall'altezza della pagina quella dell'header. Tolgo anche i 24px del padding che contengono il testo per far "respirare"
      //lo scrolling su lato inferiore
      idContainerContenuto.style.height = (idClass.offsetHeight - idContentHeader.offsetHeight) - 24 + "px";
-     console.log(idClass.offsetHeight);
-     console.log(idContentHeader.offsetHeight);
-     console.log(idContainerContenuto.style.height);
 
      this.menuCtrl.enable(false, "menuPrincipale");
-     console.log(this.myVoceMenu);
+     this.initializeBackButton();
 
     let spinnerLoading = this.loadingCtrl.create({
       });
@@ -70,7 +68,9 @@ export class MmEventiPage {
           let contenutoEventi = document.getElementById('contenuto')
           var domParser = new DOMParser()
           let doc = domParser.parseFromString(htmltxt, 'text/html')
-          doc.querySelectorAll('item').forEach((item) => {
+          const allItems = doc.querySelectorAll('item');
+          const allRealItems = Array.from(allItems);
+          allRealItems.forEach(item => {
             let divItem = document.createElement('div')
             divItem.className = 'itemFeed'
             contenutoEventi.appendChild(divItem)
@@ -91,45 +91,30 @@ export class MmEventiPage {
             let immagineItem = sottoStringaDescrizione[0]
             let testoDescrizione = document.createTextNode(sottoStringaDescrizione[1])
             periodoItem.insertAdjacentHTML('afterend',immagineItem)
-            descrizioneItem.appendChild(testoDescrizione)
-
-          })
+            descrizioneItem.appendChild(testoDescrizione)       
+          });
         })
         spinnerLoading.dismiss();
       })
-      .catch(error =>
-        console.log("errorone"))
+      .catch(() => {
+        spinnerLoading.dismiss();
+        let alert = this.alertCtrl.create({
+          title: "Purtroppo non sono riuscito a recuperare gli eventi. Si prega di riprovare più tardi, grazie.",
+          buttons: ['OK']
+        });
+        alert.present();
+      });
+  }
 
+  ionViewWillLeave() {
+    this.unregisterBackButtonAction && this.unregisterBackButtonAction();
+  }
 
-    // fetch('https://www.repubblica.it',{mode: 'no-cors'}).then((res) => {
-    //   res.text().then((htmlTxt) => {
-    //     console.log(htmlTxt)
-    //     var domParser = new DOMParser()
-    //     let doc = domParser.parseFromString(htmlTxt, 'text/html')
-    //     var feedUrl = doc.querySelector('link[type="application/rss+xml"]')
-    //     console.log(feedUrl)
-
-    //   })
-    // }).catch(() => console.error('Error in fetching the website'))
-
-
-//  fetch('http://informa.comune.bologna.it/iperbole/rss/events/25', {mode: 'no-cors'}).then((res) => {
-//       res.text().then((xmlTxt) => {
-//         console.log(res);
-//         var domParser = new DOMParser()
-//         let doc = domParser.parseFromString(xmlTxt, 'text/html')
-//         doc.querySelectorAll('item').forEach((item) => {
-//            let h1 = document.createElement('h1')
-//             h1.textContent = item.querySelector('title').textContent
-//              //document.querySelector('output').appendChild(h1)
-//              console.log(item.querySelector('title').textContent)
-//              console.log(document.querySelector('output').appendChild(h1))
-//            })
-//          })
-//     })
-
-
-
+  initializeBackButton(): void {
+    this.unregisterBackButtonAction = this.platform.registerBackButtonAction(() => {
+      this.navCtrl.push(MuseoPage, {musei: this.myMuseo, classe1: this.myMuseoClass});
+      this.navCtrl.removeView(this.navCtrl.last());
+    });
   }
 
   //Apre il side menu
